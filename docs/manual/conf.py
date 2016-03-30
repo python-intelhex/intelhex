2c9130027b450f483d766a773eee7f984510a23f
Alexander Belchenko <alexander.belchenko@gmail.com>
2016-03-31 01:56:05 +0300

    Changes from bzr: attempt to make docs' conf.py compatible with sphinx 1.2.2 (on readthedocs.org)
    bzr revno 235 (revid:alexander.belchenko@gmail.com-20150412211004-vzob5f4hojs2tw21) - 2015/04/13

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 # Options for HTML output
 # -----------------------
 
-html_theme = 'classic'
+#html_theme = 'classic'
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
